a573bc53e60385a1545a24f69597310c631c089d
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2025-11-06 17:06:25 +0500

    Update route.ts

--- src/app/api/auth/validate-session/route.ts
+++ src/app/api/auth/validate-session/route.ts
@@ -4,14 +4,17 @@ import Session from "@/models/Session";
 import { cookies } from "next/headers";
 
 export async function GET() {
-  await connectToDatabase();
+  const connected2DB  = await connectToDatabase();
+  if (!connected2DB) {
+    return Response.json({ valid: false, reason: "not connected to DB" });
+  }
   const sessionId = cookies().get("sessionId")?.value;
   if (!sessionId)
-    return Response.json({ valid: false, reason: "No session" });
+    return Response.json({ valid: false, reason: "No session get from cookies" });
 
   const session = await Session.findOne({ sessionId });
   if (!session)
-    return Response.json({ valid: false, reason: "Invalid session" });
+    return Response.json({ valid: false, reason: "Invalid session according to our database" });
 
   return Response.json({ valid: true });
 }
