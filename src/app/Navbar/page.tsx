2c1613bce0f466b750cc0e734b9da3766ae35f36
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2025-11-07 23:34:42 +0500

    Update page.tsx

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -1,27 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    // Check if there's a token in cookies
-    const token = document.cookie
-      .split(";")
-      .find((cookie) => cookie.trim().startsWith("token="));
-    setIsAuthenticated(!!token); // Set authentication state based on token presence
-  }, []);
 
   return (
     <nav className="flex justify-between">
       <div>
-        <Link href="/">Home</Link>
+        <Link href="/" className="hover:bg-[#0F6466] hover:text-white">Home</Link>
       </div>
       <div>
-        <Link href="/userData/ProfileUser">User</Link>
-        <Link href="/adminData/ProfileAdmin">Admin</Link>
+        <Link href="/userData/ProfileUser" className="hover:bg-[#0F6466] hover:text-white">Salesman</Link>
+        <Link href="/adminData/ProfileAdmin" className="hover:bg-[#0F6466] hover:text-white">Manager</Link>
       </div>
     </nav>
   );
